5e9a05f8ee420da7fcbf01fe9c9761a147188a2f
Viraj Mulik <144205690+Viraj1923@users.noreply.github.com>
2026-02-12 20:11:39 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,13 +11,13 @@ function Footer() {
           </p>
           {/* Social Media Links */}
           <div className="social-icons">
-            <a href="#" target="_blank" rel="noreferrer" title="LinkedIn">
+            <a href="" target="_blank" rel="noreferrer" title="LinkedIn">
               <i className="fab fa-linkedin-in"></i>
             </a>
             <a href="https://github.com/Onkargiri29" target="_blank" rel="noreferrer" title="GitHub">
               <i className="fab fa-github"></i>
             </a>
-            <a href="#" target="_blank" rel="noreferrer" title="Instagram">
+            <a href="" target="_blank" rel="noreferrer" title="Instagram">
               <i className="fab fa-instagram"></i>
             </a>
           </div>
